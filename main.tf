c583810995d0df504946fd5d32b21ff223d9432d
CyberarkReleaser <svc_go_ark_releaser@cyberark.com>
2026-05-11 14:15:43 +0000

    EV-00000: fix: fix: fix version

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_version = ">= 1.8.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+    idsec = {
+      source  = "cyberark/idsec"
+      version = "~> 0.2.1"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+}
+
+
 data "aws_caller_identity" "current" {}
 
 locals {
